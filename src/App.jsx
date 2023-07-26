52836d9266b401c49e0ea5f1a47cbb10cd3e22c5
Toyibat Adele <112214060+Teebarh@users.noreply.github.com>
2023-07-26 00:10:34 +0000

    Resume updates

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Toyibat Adele",
+  title: "Data Scientist & Technical Writer",
+  email: "toyibatadele@gmail.com",
+  gitHub: "Teebarh",
+  instagram: "Teebarh_",
+  linkedIn: "Toyibatadele",
+  medium: "@toyibatadele",
+  twitter: "Teebarh_",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,27 +16,27 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/Resume_1.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "Image showing a light pink background with a pair of glasses, keyboard, two pens and a book";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Mechatronics Engineering student studying at Federal University of Agriculture, Abeokuta. I'm enjoy giving machines the ability to learn, and writing about my learnings and experiences along the way'.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Microsoft Office",
+  "MySQL",
+  "Python",
+  "Git and GitHub",
+  "Markdown",
+  "Article Writing",
 ];
 
 /**
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about solving problems in new creative ways to drive innovation.";
 
 const About = () => {
   return (
